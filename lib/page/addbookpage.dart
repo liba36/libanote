0770d6052a9a2ea56108c753ef6707d98e58e12f
liba36 <sunr1995@qq.com>
2019-04-02 21:06:14 +0800

    create book list

--- lib/page/addbookpage.dart
+++ lib/page/addbookpage.dart
@@ -9,9 +9,9 @@ class addbookpage extends StatefulWidget {
   }
 }
 
-
 class _addbookState extends State<addbookpage> {
   Color _defulcolor = Color.fromRGBO(74, 169, 170, 1);
+  String _title = "新的本子哟";
   int _selectvalue;
 
   List<DropdownMenuItem> _getDdmItem() {
@@ -39,106 +39,124 @@ class _addbookState extends State<addbookpage> {
       _selectvalue = T;
     });
   }
+  _changeTitle(T){
+    if("" !=  T) {
+      setState(() {
+        _title = T;
+      });
+    }
+    else
+      {
+        setState(() {
+          _title = "新的本子哟";
+        });
+      }
+  }
 
-  Widget _getimage()
-  {
-    return  new Image.asset('assets/images/diary-y.png',
+  Widget _getimage() {
+    return new Image.asset(
+      'assets/images/diary-y.png',
       height: 153.55,
       width: 109.25,
-      fit: BoxFit.fill
-      ,
+      fit: BoxFit.fill,
     );
   }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-        backgroundColor:  Colors.white,
-        appBar: new AppBar(
-          leading: IconButton(icon: Icon(Icons.arrow_back,color: _defulcolor,), onPressed: () {Navigator.pop(context);}),
-          backgroundColor: Colors.white,
-          elevation: 0,
-        ),
-        body: SingleChildScrollView(
-          child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin:
-                    EdgeInsets.only(left: 36, right: 36, top: 10, bottom: 10),
-                padding: EdgeInsets.all(10),
-                constraints: BoxConstraints(
-                    //子部件最小高度
-                    minHeight: 200,
-                  minWidth: 200
-                ),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(
-                      color: _defulcolor,
-                      width: 1,
-                    )),
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    _getimage(),
-                    Text("asdsdasdsa"),
-                  ],
-                ),
-              ),
-              Container(
-                  margin: EdgeInsets.only(left: 20, right: 20),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Text(
-                        "本子类型：",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      DropdownButton(
-                        hint: Text(
-                          "请选择本子类型",
-                        ),
-                        items: _getDdmItem(),
-                        onChanged: _changeType,
-                        value: _selectvalue,
-                      ),
-                    ],
+      backgroundColor: Colors.white,
+      appBar: new AppBar(
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: _defulcolor,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 36, right: 36, top: 10, bottom: 10),
+              padding: EdgeInsets.all(10),
+              constraints: BoxConstraints(
+                  //子部件最小高度
+                  minHeight: 200,
+                  minWidth: 200),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  border: Border.all(
+                    color: _defulcolor,
+                    width: 1,
                   )),
-              Container(
-                margin:
-                    EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 10),
-                child: TextField(
-                  maxLength: 20,
-                  //光标
-                  cursorColor: _defulcolor,
-                  // cursorWidth: 10,
-                  style: TextStyle(fontSize: 16),
-                  keyboardType: TextInputType.text,
-                  maxLines: null,
-                  decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: _defulcolor, width: 2)),
-                    contentPadding: EdgeInsets.all(5.0),
-                    // icon: Icon(Icons.title),
-                    labelText: '请输入名称',
-                    labelStyle: TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey,
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  _getimage(),
+                  Text(_title),
+                ],
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      "本子类型：",
+                      style: TextStyle(fontSize: 16),
                     ),
+                    DropdownButton(
+                      hint: Text(
+                        "请选择本子类型",
+                      ),
+                      items: _getDdmItem(),
+                      onChanged: _changeType,
+                      value: _selectvalue,
+                    ),
+                  ],
+                )),
+            Container(
+              margin: EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 10),
+              child: TextField(
+                maxLength: 20,
+                //光标
+                cursorColor: _defulcolor,
+                // cursorWidth: 10,
+                style: TextStyle(fontSize: 16),
+                keyboardType: TextInputType.text,
+                maxLines: null,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: _defulcolor, width: 2)),
+                  contentPadding: EdgeInsets.all(5.0),
+                  // icon: Icon(Icons.title),
+                  labelText: '请输入名称',
+                  labelStyle: TextStyle(
+                    fontSize: 15,
+                    color: Colors.grey,
                   ),
                 ),
+                onChanged: _changeTitle,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
       floatingActionButton: FloatingActionButton(
-      backgroundColor: _defulcolor,
-     // onPressed: _editdiarypage1,
-      tooltip: 'Increment', //长按显示的文字
-      child: Icon(Icons.done),
-    ),
+        backgroundColor: _defulcolor,
+        // onPressed: _editdiarypage1,
+        tooltip: '提交', //长按显示的文字
+        child: Icon(Icons.done),
+      ),
     );
   }
 }
--- lib/page/booklistpage.dart
+++ lib/page/booklistpage.dart
@@ -14,252 +14,64 @@ class booklistpage extends StatefulWidget {
 class _booklistState extends State<booklistpage> {
   sqlhelper _sqlhlper = new sqlhelper();
   Color _defulcolor = Color.fromRGBO(74, 169, 170, 1);
+  String _title = "defaut";
 
   List<Widget> test() {
-    return <Widget>[
+    List<Widget> list = new List();
+    for (int i = 0; i < 5; i++) {
+      list.add(_getGridItem());
+    }
+    return list;
+  }
 
-      GestureDetector(
-        onTap: _editdiarypage,
-        child:Column(
+  Widget _getGridItem() {
+    return new GestureDetector(
+        onLongPress: _editdiarypage,
+        child: Column(
           children: <Widget>[
-            new Image.asset('assets/images/diary-y.png',
+            new Image.asset(
+              'assets/images/diary-y.png',
               height: 92.13,
               width: 65.55,
               fit: BoxFit.fill,
             ),
-            Text("nidemingzi"),
+            Text(_title),
           ],
-        )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ), GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ), GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ), GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-      GestureDetector(
-          onTap: _editdiarypage,
-          child:Column(
-            children: <Widget>[
-              new Image.asset('assets/images/diary-y.png',
-                height: 92.13,
-                width: 65.55,
-                fit: BoxFit.fill,
-              ),
-              Text("nidemingzi"),
-            ],
-          )
-      ),
-
-
-
-
-
-
-    ];
-  }
-
-  Widget _getGridItem() {
-    return new Column(
-      children: <Widget>[
-        Text("asdsda"),
-        RaisedButton(
-          // onPressed: _editdiarypage(),
-        ),
-      ],
-    );
+        ));
   }
 
   _editdiarypage() {
-    Navigator.push(
+  /*  Navigator.push(
         context,
         new MaterialPageRoute(
           builder: (context) =>
-          new editdiarypage("touyici", "dierci", _sqlhlper, _defulcolor),
-        ));
+              new editdiarypage("touyici", "dierci", _sqlhlper, _defulcolor),
+        ));*/
+  setState(() {
+    _title="edit";
+  });
+
   }
 
-  _addnewbook(){
+  _addnewbook() {
     Navigator.push(
         context,
         new MaterialPageRoute(
-          builder: (context) => new addbookpage(
-          ),
+          builder: (context) => new addbookpage(),
         ));
   }
 
-
-  Future<Null> _refresh() async {
-
-  }
-
-
+  Future<Null> _refresh() async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: new AppBar(
+        title: new Text("liba36"),
+        backgroundColor: _defulcolor,
+      ),
       body: Container(
-        margin: EdgeInsets.only(left: 10, right: 10,top: 20),
+        margin: EdgeInsets.only(left: 10, right: 10, top: 20),
         child: RefreshIndicator(
           onRefresh: _refresh,
           child: GridView.count(
@@ -277,10 +89,9 @@ class _booklistState extends State<booklistpage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed:_addnewbook,
+        onPressed: _addnewbook,
         backgroundColor: _defulcolor,
         child: Icon(Icons.add),
-
       ),
     );
   }
