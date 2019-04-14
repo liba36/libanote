e9fc3e3a6d2df36c6e97d62d59232598d7b44462
liba36 <sunr1995@qq.com>
2019-04-14 15:49:29 +0800

    调整颜色

---
+++ lib/page/diaryviewpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class diaryviewpage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
+
+class _diaryviewState extends State<diaryviewpage>
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
--- lib/view/diarycard.dart
+++ lib/view/diarycard.dart
@@ -4,7 +4,8 @@ import 'package:liba_note/model/diary.dart';
 class diarycard extends StatefulWidget {
   diary DiaryInstance;
   Color ColorDeful;
-  diarycard(this.DiaryInstance,this.ColorDeful);
+
+  diarycard(this.DiaryInstance, this.ColorDeful);
 
   @override
   State<StatefulWidget> createState() {
@@ -16,49 +17,60 @@ class diarycard extends StatefulWidget {
 class _diarycardState extends State<diarycard> {
   // diarycard(this._diary);
 
+  _clik()
+  {
+
+  }
+
   Widget build(BuildContext context) {
-    return new Container(
-      margin: EdgeInsets.only(right: 10, left: 10),
-      child: Card(
-        color: Color.fromRGBO(243, 246, 240, 1),
+    return new GestureDetector(
+      onTap: _clik,
+      child:  Card(
         elevation: 3,
-        margin: EdgeInsets.only(top: 10),
+        margin: EdgeInsets.all(10),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
                 color: widget.ColorDeful,
                 alignment: Alignment.center,
-                padding: EdgeInsets.only(top: 10,bottom: 10,left: 3,right: 3),
-                child: Text(widget.DiaryInstance.title,style: TextStyle(fontSize: 18,color: Colors.white),)),
+                padding: EdgeInsets.only(top: 3, bottom: 3, left: 3, right: 3),
+                child: Text(
+                  widget.DiaryInstance.title,
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                )),
             Container(
-              margin: EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 10),
-              //color: Color.fromRGBO(250, 250, 250, 1),
-              child: new Row(
+              margin: EdgeInsets.all(5),
+              child: Text(
+                widget.DiaryInstance.context,
+                style: TextStyle(
+                    color: Color.fromRGBO(122, 123, 122, 1),
+                    fontSize: 15,
+                    wordSpacing: 1.36,
+                    letterSpacing: 0.36,
+                    height: 1.36),
+                softWrap: true,
+                maxLines: 5,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(3),
+              alignment: Alignment.centerLeft,
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                // mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Container(
-                    child: Text(
-                      widget.DiaryInstance.edittime.year.toString(),
-                      style: TextStyle(color: Color.fromRGBO(173, 185, 190, 1)),
-                    ),
-                    margin: EdgeInsets.only(bottom: 10, top: 5, left: 20),
-                  ),
-                  Expanded(
-
-                    child: Container(
-                      margin: EdgeInsets.only(left: 10, right: 5, bottom: 5),
-                      child: Text(
-                        widget.DiaryInstance.context,
-                        softWrap: true,
-                        maxLines: 4,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ),
+                  Text(widget.DiaryInstance.context.length.toString(),style:TextStyle(fontSize: 10,color: Color.fromRGBO(136, 136, 136, 1))),
+                  Text(widget.DiaryInstance.edittime.year.toString()+"/"
+                      +widget.DiaryInstance.edittime.month.toString()+"/"
+                      +widget.DiaryInstance.edittime.day.toString(),
+                    style: TextStyle(fontSize: 10,color: Color.fromRGBO(136, 136, 136, 1)),
+                  )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
