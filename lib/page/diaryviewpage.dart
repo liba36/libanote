5cf477769e19fbdaa4b17f6010ab95a0abae891c
liba36 <sunr1995@qq.com>
2019-04-18 10:41:37 +0800

    调整diaryview界面

--- lib/page/diaryviewpage.dart
+++ lib/page/diaryviewpage.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:liba_note/model/diary.dart';
 
-class diaryviewpage extends StatefulWidget{
+class diaryviewpage extends StatefulWidget {
   Color ColorDeful;
   diary DiaryInstance;
-  diaryviewpage(this.ColorDeful,this.DiaryInstance);
+
+  diaryviewpage(this.ColorDeful, this.DiaryInstance);
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -12,15 +14,25 @@ class diaryviewpage extends StatefulWidget{
   }
 }
 
-class _diaryviewState extends State<diaryviewpage>
-{
+class _diaryviewState extends State<diaryviewpage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: new AppBar(
-        title: new Text(widget.DiaryInstance.edittime.toString()),
+        title: new Text(
+          widget.DiaryInstance.edittime.year.toString() +
+              "年" +
+              widget.DiaryInstance.edittime.month.toString() +
+              "月" +
+              widget.DiaryInstance.edittime.day.toString() +
+              "日 " +
+              widget.DiaryInstance.edittime.hour.toString() +
+              ":" +
+              widget.DiaryInstance.edittime.second.toString(),
+          style: TextStyle(fontSize: 16, color: Colors.grey),
+        ),
         elevation: 0,
         backgroundColor: Colors.white,
         leading: IconButton(
@@ -35,12 +47,22 @@ class _diaryviewState extends State<diaryviewpage>
       body: Column(
         children: <Widget>[
           Container(
-            margin: EdgeInsets.only(left: 18,right: 18),
+            padding: EdgeInsets.only(bottom: 10),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: widget.ColorDeful,
+                  width: 1
+                )
+              )
+            ),
+            margin: EdgeInsets.only(left: 18, right: 18,top: 20),
             alignment: Alignment.centerLeft,
-            child: Text(widget.DiaryInstance.title,
+            child: Text(
+              widget.DiaryInstance.title,
               style: TextStyle(
-                fontSize: 25,
-
+                fontSize: 23,
+                color: Color.fromRGBO(66, 66, 66, 1)
               ),
               softWrap: true,
               maxLines: null,
@@ -48,12 +70,11 @@ class _diaryviewState extends State<diaryviewpage>
           ),
           Container(
             alignment: Alignment.centerLeft,
-            margin: EdgeInsets.only(left: 17,right: 17,top: 20),
-            child: Text(widget.DiaryInstance.context,
-            style: TextStyle(
-              fontSize: 20
-            ),
-            softWrap: true,
+            margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: Text(
+              widget.DiaryInstance.context,
+              style: TextStyle(fontSize: 18,color: Color.fromRGBO(99, 99, 99, 1)),
+              softWrap: true,
               maxLines: null,
             ),
           )
@@ -61,8 +82,8 @@ class _diaryviewState extends State<diaryviewpage>
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color.fromRGBO(74, 169, 170, 1),
-        child: Icon(Icons.add),
-       // onPressed: _addDiaryPage,
+        child: Icon(Icons.edit),
+        // onPressed: _addDiaryPage,
       ),
     );
   }
