3fcdc09a4d9e9ce058d786e9e963ba66c3382c71
liba36 <sunr1995@qq.com>
2019-08-09 15:03:18 +0800

    添加androidx支持

--- lib/page/accountlistpage.dart
+++ lib/page/accountlistpage.dart
@@ -4,7 +4,6 @@ import 'package:liba_note/utils/sqlhelper.dart';
 
 import 'package:liba_note/page/editaccountpage.dart';
 
-
 class accountistpage extends StatefulWidget {
   sqlhelper SQLUtils;
   int BookId;
@@ -21,29 +20,51 @@ class _accountlistState extends State<accountistpage> {
 
   Widget build(BuildContext context) {
     return new WillPopScope(
-        child: Scaffold(
-          backgroundColor: Colors.white,
-         // appBar: _getappbar(_CheckBoxDisplay),
-          body: _getBody(),
-         // floatingActionButton: _getFloatButtopn(_CheckBoxDisplay),
-        ),
-        //onWillPop: _back
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: _getAppBar(),
+        body: _getBody(),
+        floatingActionButton: _getFloatButtopn(),
+      ),
+      //onWillPop: _back
     );
   }
 
-  _getBody(){
-    return Container(
-
-    );
+  _getBody() {
+    return Container();
   }
 
-  _addAccount()
-  {
+  _addAccount() {
     Navigator.push(
         context,
         new MaterialPageRoute(
-          builder: (context) => new editaccountpage(1,widget.SQLUtils, _defaultcolor, null),
-        )).then((data) {
-    });
+          builder: (context) =>
+              new editaccountpage(1, widget.SQLUtils, _defaultcolor, null),
+        )).then((data) {});
+  }
+
+  _getAppBar() {
+    return new PreferredSize(
+        child: new AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+                color: Color.fromRGBO(74, 169, 170, 1),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          elevation: 0,
+        ),
+        preferredSize: Size.fromHeight(36));
+  }
+
+  _getFloatButtopn() {
+    return FloatingActionButton(
+      backgroundColor: Color.fromRGBO(74, 169, 170, 1),
+      child: Icon(Icons.add),
+      onPressed: _addAccount,
+    );
   }
 }
--- lib/utils/sqlhelper.dart
+++ lib/utils/sqlhelper.dart
@@ -3,7 +3,6 @@ import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'package:path/path.dart';
 import 'dart:async';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:liba_note/model/diary.dart';
 import 'package:liba_note/model/book.dart';
 
